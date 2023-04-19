b19207ddec9e9b52f055f12a6f3d3aa164b06a0c
Danny <mr.unknow47@gmail.com>
2023-04-19 18:38:02 +0800

    user login function

--- api/main_test.go
+++ api/main_test.go
@@ -3,15 +3,23 @@ package api
 import (
 	"github.com/gin-gonic/gin"
 	db "github.com/kys20548/simple_bank/db/sqlc"
+	"github.com/kys20548/simple_bank/util"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
 
 func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+		}
 
-	server := NewServer(store)
+		server, err := NewServer(config, store)
+		require.NoError(t, err)
 
-	return server
+		return server
 }
 
 func TestMain(m *testing.M) {
--- util/config.go
+++ util/config.go
@@ -4,12 +4,16 @@ import (
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 	"log"
+	"time"
 )
 
 type Config struct {
 	DBDriver      string `mapstructure:"DB_DRIVER"`
 	DBSource      string `mapstructure:"DB_SOURCE"`
 	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+
 }
 
 // LoadConfig reads configuration from file or environment variables.
