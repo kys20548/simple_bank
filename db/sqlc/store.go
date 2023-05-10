d3e47db5b96c19573df2c095664a677eb1b93cb3
kys20548 <kys20548@livemail.tw>
2023-05-10 14:25:09 +0800

    fix

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -42,22 +42,22 @@ func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) erro
 
 type TransferTxParams struct {
 	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:to_account_id`
-	Amount        int64 `json:amount`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
 }
 
 type TransferTxResult struct {
-	Transfer    Transfer `json:transfer`
-	FromAccount Account  `json:from_account`
-	ToAccount   Account  `json:to_account`
-	FromEntry   Entry    `json:from_entry`
-	ToEntry     Entry    `json:to_entry`
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
 }
 
-func (stroe *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
-	err := stroe.execTx(ctx, func(q *Queries) error {
+	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
