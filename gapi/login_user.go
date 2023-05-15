b323f42c426bed2eaa376c314f76e0f9b23b88f3
kys20548 <kys20548@livemail.tw>
2023-05-15 14:31:09 +0800

    update partial column

--- gapi/login_user.go
+++ gapi/login_user.go
@@ -6,12 +6,19 @@ import (
 	db "github.com/kys20548/simple_bank/db/sqlc"
 	"github.com/kys20548/simple_bank/pb"
 	"github.com/kys20548/simple_bank/util"
+	"github.com/kys20548/simple_bank/val"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server Server) Loginuser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -61,4 +68,16 @@ func (server Server) Loginuser(ctx context.Context, req *pb.LoginUserRequest) (*
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
